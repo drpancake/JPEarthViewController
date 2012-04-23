c84e329f74ee500e5e6512066f371eef929edf3c
James Potter <james.potter@gmail.com>
2012-04-23 11:17:31 +0100

    GE plugin loads

--- JPEarthViewController.m
+++ JPEarthViewController.m
@@ -9,7 +9,21 @@
 {
     webView = [[WebView alloc] initWithFrame:CGRectZero];
     self.webView.frameLoadDelegate = self;
+    
+    // Enable plugins
+    WebPreferences *prefs = webView.preferences;
+    [prefs setPlugInsEnabled:YES];
+    
+    // Google's JS API for the GE plugin does some user-agent sniffing - make it think we're Safari
+    [webView setCustomUserAgent:@"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.55.3 "
+                                 "(KHTML, like Gecko) Version/5.1.5 Safari/534.55.3"];
+    
     self.view = self.webView;
+    
+    // Navigate to 'earth.html'
+    WebFrame *webFrame = [self.webView mainFrame];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"earth" ofType:@"html"];
+    [webFrame loadRequest:[NSURLRequest requestWithURL:[NSURL fileURLWithPath:path]]];
 }
 
 #pragma mark -
